dbb279ffb982abd9eaafdd9f56286257f1f82e06
OwaisZaidi <owaiszaidi@aplosinnovations.com>
2016-05-12 19:54:51 +0500

    added api controller

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,166 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Input;
+use Illuminate\Http\Response as IlluminateResponse;
+use DB;
+
+
+
+class ApiController extends Controller{
+
+
+    public function register_patient(Request $request){
+
+        $first_name = $request->input('first_name');
+
+        $middle_name = $request->input('middle_name');
+
+        $last_name = $request->input('last_name');
+
+        $date_of_birth = $request->input('date_of_birth');
+
+        $age = $request->input('age');
+
+        $martial_status = $request->input('martial_status');
+
+        $religion = $request->input('religion');
+
+        $father_firstname = $request->input('father_firstname');
+
+        $father_middlename= $request->input('father_middlename');
+
+        $father_lastname = $request->input('father_lastname');
+
+        $mother_firstname = $request->input('mother_firstname');
+
+        $mother_middlename = $request->input('mother_middlename');
+
+        $mother_lastname= $request->input('mother_lastname');
+
+        $refered_name = $request->input('refered_name');
+
+        $refered_email = $request->input('refered_email');
+
+        $refered_phone = $request->input('refered_phone');
+
+        $patient_unit_number = $request->input('patient_unit_number');
+
+        $currentdatetime = date("Y-m-d  H:i:s");
+
+
+        DB::table('patients')->insert(
+            ['first_name'=> $first_name,
+                'middle_name' => $middle_name,
+                'last_name' => $last_name,
+                'date_of_birth' => $date_of_birth,
+                'age' => $age,
+                'martial_status' => $martial_status,
+                'religion' => $religion,
+                'father_firstname' => $father_firstname,
+                'father_middlename' => $father_middlename,
+                'father_lastname' => $father_lastname,
+                'mother_firstname' => $mother_firstname,
+                'mother_middlename' => $mother_middlename,
+                'mother_lastname' => $mother_lastname,
+                'refered_name' => $refered_name,
+                'refered_email' => $refered_email,
+                'refered_phone' => $refered_phone,
+                'patient_unit_number' => $patient_unit_number,
+                'created_at' => $currentdatetime
+
+            ]
+        );
+
+        $patient_id = DB::getPdo()->lastInsertId();
+
+
+        $address_type = $request->input('address_type');
+
+        $phone_number = $request->input('phone_number');
+
+        $mobile_number= $request->input('mobile_number');
+
+        $house_number = $request->input('house_number');
+
+        $street = $request->input('street');
+
+        $city = $request->input('city');
+
+        $state= $request->input('state');
+
+        $country = $request->input('country');
+
+        $local_goverment_area = $request->input('local_goverment_area');
+
+        $postal_code = $request->input('postal_code');
+
+
+        DB::table('patient_address')->insert(
+            [   'patient_id'=> $patient_id,
+                'address_type'=> $address_type,
+                'phone_number' => $phone_number,
+                'mobile_number' => $mobile_number,
+                'house_number' => $house_number,
+                'street' => $street,
+                'city' => $city,
+                'state' => $state,
+                'country' => $country,
+                'local_goverment_area' => $local_goverment_area,
+                'postal_code'=> $postal_code,
+                'created_at' => $currentdatetime
+
+            ]
+        );
+
+
+
+        return response()->json(['status' => true, 'message' => 'Patient Registered Successfully']);
+
+
+    }
+
+    public function register_user(Request $request){
+
+
+        $username = $request->input('name');
+
+        $password = $request->input('password');
+
+        $password_user = md5($password);
+
+        $user_role_id = $request->input('role_id');
+
+        $currentdatetime = date("Y-m-d  H:i:s");
+
+
+        DB::table('users')->insert(
+            [   'name'=> $username,
+                'password'=> $password_user,
+                'role_id' => $user_role_id,
+                'created_at' => $currentdatetime
+
+            ]
+        );
+
+
+        $user_id = DB::getPdo()->lastInsertId();
+
+
+        $user = DB::table('users')
+            ->select(DB::raw('name'))
+            ->where('id', $user_id)
+            ->first();
+
+
+
+        return response()->json(['status' => true, 'data'=>$user]);
+
+
+    }
+
+
+}
