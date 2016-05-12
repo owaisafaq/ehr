d544cfe594e1d465430a37b17f5e9b2740a0b7fb
OwaisZaidi <owaiszaidi@aplosinnovations.com>
2016-05-12 22:24:18 +0500

    added jwt token authentication

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2,18 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Exception\HttpResponseException;
+use JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Input;
 use Illuminate\Http\Response as IlluminateResponse;
+
 use DB;
 
 
+class ApiController extends Controller
+{
 
-class ApiController extends Controller{
 
-
-    public function register_patient(Request $request){
+    public function register_patient(Request $request)
+    {
 
         $first_name = $request->input('first_name');
 
@@ -31,7 +36,7 @@ class ApiController extends Controller{
 
         $father_firstname = $request->input('father_firstname');
 
-        $father_middlename= $request->input('father_middlename');
+        $father_middlename = $request->input('father_middlename');
 
         $father_lastname = $request->input('father_lastname');
 
@@ -39,7 +44,7 @@ class ApiController extends Controller{
 
         $mother_middlename = $request->input('mother_middlename');
 
-        $mother_lastname= $request->input('mother_lastname');
+        $mother_lastname = $request->input('mother_lastname');
 
         $refered_name = $request->input('refered_name');
 
@@ -53,7 +58,7 @@ class ApiController extends Controller{
 
 
         DB::table('patients')->insert(
-            ['first_name'=> $first_name,
+            ['first_name' => $first_name,
                 'middle_name' => $middle_name,
                 'last_name' => $last_name,
                 'date_of_birth' => $date_of_birth,
@@ -82,7 +87,7 @@ class ApiController extends Controller{
 
         $phone_number = $request->input('phone_number');
 
-        $mobile_number= $request->input('mobile_number');
+        $mobile_number = $request->input('mobile_number');
 
         $house_number = $request->input('house_number');
 
@@ -90,7 +95,7 @@ class ApiController extends Controller{
 
         $city = $request->input('city');
 
-        $state= $request->input('state');
+        $state = $request->input('state');
 
         $country = $request->input('country');
 
@@ -100,8 +105,8 @@ class ApiController extends Controller{
 
 
         DB::table('patient_address')->insert(
-            [   'patient_id'=> $patient_id,
-                'address_type'=> $address_type,
+            ['patient_id' => $patient_id,
+                'address_type' => $address_type,
                 'phone_number' => $phone_number,
                 'mobile_number' => $mobile_number,
                 'house_number' => $house_number,
@@ -110,12 +115,11 @@ class ApiController extends Controller{
                 'state' => $state,
                 'country' => $country,
                 'local_goverment_area' => $local_goverment_area,
-                'postal_code'=> $postal_code,
+                'postal_code' => $postal_code,
                 'created_at' => $currentdatetime
 
             ]
         );
-
 
 
         return response()->json(['status' => true, 'message' => 'Patient Registered Successfully']);
@@ -123,10 +127,13 @@ class ApiController extends Controller{
 
     }
 
-    public function register_user(Request $request){
+    public function register_user(Request $request)
+    {
 
 
         $username = $request->input('name');
+
+        $email = $request->input('email');
 
         $password = $request->input('password');
 
@@ -136,10 +143,25 @@ class ApiController extends Controller{
 
         $currentdatetime = date("Y-m-d  H:i:s");
 
+        $users = DB::table('users')
+            ->select(DB::raw('name'))
+            ->where('email', $email)
+            ->get();
+
+        if (count($users) > 0) {
+
+            return response()->json(['status' => false, 'message' => "Email Exists Already"]);
+
+            exit;
+
+
+        }
+
 
         DB::table('users')->insert(
-            [   'name'=> $username,
-                'password'=> $password_user,
+            ['name' => $username,
+                'email' => $email,
+                'password' => $password_user,
                 'role_id' => $user_role_id,
                 'created_at' => $currentdatetime
 
@@ -151,13 +173,44 @@ class ApiController extends Controller{
 
 
         $user = DB::table('users')
-            ->select(DB::raw('name'))
+            ->select(DB::raw('name,id'))
             ->where('id', $user_id)
             ->first();
 
+        $token = JWTAuth::fromUser($user);
 
 
-        return response()->json(['status' => true, 'data'=>$user]);
+        return response()->json(['status' => true, 'message' => "User Registered Successfully", 'data' => $user, 'token' => $token]);
+
+
+    }
+
+    public function user_login(Request $request)
+    {
+
+        $username = $request->input('name');
+
+        $password = $request->input('password');
+
+        $password_user = md5($password);
+
+        $user = DB::table('users')
+            ->select(DB::raw('id'))
+            ->where('name', $username)
+            ->where('password', $password_user)
+            ->get();
+
+        if (count($user) == 1) {
+
+            $token = JWTAuth::fromUser($user[0]);
+
+            return response()->json(['status' => true, 'data' => $user[0], 'token' => $token]);
+
+        } else {
+
+            return response()->json(['status' => false, 'message' => 'invalid user']);
+
+        }
 
 
     }
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -24,6 +24,13 @@ $app = new Laravel\Lumen\Application(
 );
 
  $app->withFacades();
+ $app->configure('jwt');
+
+class_alias('Tymon\JWTAuth\Facades\JWTAuth', 'JWTAuth');
+/** This gives you finer control over the payloads you create if you require it.
+ *  Source: https://github.com/tymondesigns/jwt-auth/wiki/Installation
+ */
+class_alias('Tymon\JWTAuth\Facades\JWTFactory', 'JWTFactory'); // Optional
 
 // $app->withEloquent();
 
@@ -48,6 +55,22 @@ $app->singleton(
     App\Console\Kernel::class
 );
 
+
+$app->singleton(
+    Illuminate\Auth\AuthManager::class,
+    function ($app) {
+        return $app->make('auth');
+    }
+);
+
+$app->singleton(
+    Illuminate\Cache\CacheManager::class,
+    function ($app) {
+        return $app->make('cache');
+    }
+);
+
+
 /*
 |--------------------------------------------------------------------------
 | Register Middleware
@@ -63,9 +86,11 @@ $app->singleton(
 //    App\Http\Middleware\ExampleMiddleware::class
 // ]);
 
-// $app->routeMiddleware([
-//     'auth' => App\Http\Middleware\Authenticate::class,
-// ]);
+ $app->routeMiddleware([
+     'auth' => App\Http\Middleware\Authenticate::class,
+     'jwt.auth'    => Tymon\JWTAuth\Middleware\GetUserFromToken::class,
+     'jwt.refresh' => Tymon\JWTAuth\Middleware\RefreshToken::class,
+ ]);
 
 /*
 |--------------------------------------------------------------------------
@@ -77,6 +102,9 @@ $app->singleton(
 | totally optional, so you are not required to uncomment this line.
 |
 */
+
+// JWTAuth Dependencies
+$app->register(Tymon\JWTAuth\Providers\JWTAuthServiceProvider::class);
 
 // $app->register(App\Providers\AppServiceProvider::class);
 // $app->register(App\Providers\AuthServiceProvider::class);
@@ -92,6 +120,9 @@ $app->singleton(
 | can respond to, as well as the controllers that may handle them.
 |
 */
+
+
+
 
 $app->group(['namespace' => 'App\Http\Controllers'], function ($app) {
     require __DIR__.'/../app/Http/routes.php';
