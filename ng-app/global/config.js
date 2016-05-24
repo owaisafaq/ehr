c7f3ad07814a7679eceac9f0bc25b1a903286e61
Muzammil Ahmed <muzammilahmed@aplosinnovations.com>
2016-05-24 12:33:25 +0500

    Files added into angular

---
+++ ng-app/global/config.js
@@ -0,0 +1,6 @@
+//var serverPath = "http://131.107.100.10/ehr/public/api/";
+var serverPath = "http://demoz.online/ehr/public/api/";
+
+var errorMessages = {
+	"authFailed": "Email or Password is Invalid"
+};
