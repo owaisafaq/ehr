f911fec7cd5919bc63f28a189bf4e58135ed47fa
Muzammil Ahmed <muzammilahmed@aplosinnovations.com>
2016-05-26 12:41:09 +0500

    Validated alphabetics and numeric field, corrected naming convention.

---
+++ ng-app/controllers/clinicaldocumentationclinicprogressnote.js
@@ -0,0 +1,5 @@
+var AppEHR = angular.module('AppEHR');
+
+AppEHR.controller('clinicalDocumentationClinicProgressNote', ['$scope', '$rootScope', function($scope, rootScope){
+	$rootScope.pageTitle = "EHR - clinical Documentation Clinic Progress Note";
+}]);
