a6dcfe759b1620782e1e7c2a46e976c1e95de7a3
muttahir1 <muttahirrasheed@aplosinnovations.com>
2016-05-30 21:46:43 -0700

    lab report haematology html

---
+++ ng-app/controllers/labReportHaematology.js
@@ -0,0 +1,5 @@
+var AppEHR = angular.module('AppEHR');
+
+AppEHR.controller('labReportHaematology', ['$scope', '$rootScope', function($scope, $rootScope){
+	$rootScope.pageTitle = "EHR - Lab Report Haematology";
+}]);
