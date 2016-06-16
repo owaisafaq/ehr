65c758f50c8a0933e67d9153bc52410bc707ec5c
muttahir1 <muttahirrasheed@aplosinnovations.com>
2016-06-16 11:46:08 -0700

    last updated with fixes and medication view page

--- ng-app/global/factories.js
+++ ng-app/global/factories.js
@@ -247,15 +247,15 @@ AppEHR.factory("UpdateEncounter", function ($resource) {
 });
 AppEHR.factory("GetOneEncounter", function ($resource) {
     function getResource(params, body) {
-        var res2 = $resource(serverPath +  '', params, {
-            save: {method: 'POST'}
+        var res2 = $resource(serverPath +  'visit_details', params, {
+            get: {method: 'GET'}
         });
         return res2;
     }
     var patientRegistrationEmployer = {
-        save:function(params,body,success) {
+        get:function(params,body,success) {
           var res = getResource(params, body);  
-          return res.save(params,body,success);
+          return res.get(params,body,success);
         }
     };
     return patientRegistrationEmployer;
@@ -331,3 +331,18 @@ AppEHR.service('fileUpload', ['$http', function ($http) {
         });
     }
 }]);
+AppEHR.factory("GetPatientMedications", function ($resource) {
+    function getResource(params, body) {
+        var res2 = $resource(serverPath +  'patient_medications', params, {
+            get: {method: 'GET'}
+        });
+        return res2;
+    }
+    var patientRegistrationEmployer = {
+        get:function(params,body,success) {
+          var res = getResource(params, body);  
+          return res.get(params,body,success);
+        }
+    };
+    return patientRegistrationEmployer;
+});
