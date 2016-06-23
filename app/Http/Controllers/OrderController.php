701912682dbe27bb5882216d6113c5aff9cdc9c1
OwaisZaidi <owaiszaidi@aplosinnovations.com>
2016-06-24 01:27:57 +0500

    updated code

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -81,7 +81,7 @@ class OrderController extends Controller
             ->leftJoin('lab_order_tests', 'lab_order_tests.lab_order_id', '=', 'lab_orders.id')
             ->leftJoin('lab_tests', 'lab_tests.id', '=', 'lab_order_tests.lab_test')
             ->leftJoin('maritial_status', 'maritial_status.id', '=', 'patients.marital_status')
-            ->where('lab_orders.status',1)
+            ->where('lab_orders.status', 1)
             ->groupby('lab_orders.id')
             ->get();
 
@@ -105,18 +105,77 @@ class OrderController extends Controller
 
         }
         foreach ($orders as $key => $order_tests) {
-           $tests = DB::table('lab_tests')
-                ->select(DB::raw('lab_tests.name as test_name,lab_tests.cost'))
+            $tests = DB::table('lab_tests')
+                ->select(DB::raw('lab_tests.name as test_name,lab_tests.cost,priority'))
                 ->leftJoin('lab_order_tests', 'lab_order_tests.lab_test', '=', 'lab_tests.id')
                 ->where('lab_order_tests.lab_order_id', $order_tests->id)
                 ->get();
-           $orders[$key]->lab_tests = $tests;
-           $test_cost = DB::table('lab_tests')
-                           ->select(DB::raw('IFNULL(SUM(lab_tests.cost),0) as cost'))
-                           ->leftJoin('lab_order_tests', 'lab_order_tests.lab_test', '=', 'lab_tests.id')
-                           ->where('lab_order_tests.lab_order_id', $order_tests->id)
-                           ->first();
-           $orders[$key]->total_cost = $test_cost->cost;
+
+            $test_cost = DB::table('lab_tests')
+                ->select(DB::raw('IFNULL(SUM(lab_tests.cost),0) as cost,count(lab_order_tests.lab_test) as totaltests'))
+                ->leftJoin('lab_order_tests', 'lab_order_tests.lab_test', '=', 'lab_tests.id')
+                ->where('lab_order_tests.lab_order_id', $order_tests->id)
+                ->first();
+
+            $orders[$key]->total_cost = $test_cost->cost;
+            $orders[$key]->total_test = $test_cost->totaltests;
+
+
+            $orders[$key]->lab_tests = $tests;
+
+            //$apetizer_product_items[]=$product_item;
+        }
+
+        return response()->json(['status' => true, 'data' => $orders]);
+        $orders = DB::table('lab_orders')
+            ->select(DB::raw('lab_orders.id,lab_orders.patient_id,patients.first_name as patient_name,lab_orders.order_status,labs.name as lab_name,patients.age,patients.marital_status,patients.sex,maritial_status.name as marital_status'))
+            ->leftJoin('patients', 'lab_orders.patient_id', '=', 'patients.id')
+            ->leftJoin('labs', 'labs.id', '=', 'lab_orders.lab')
+            ->leftJoin('lab_order_tests', 'lab_order_tests.lab_order_id', '=', 'lab_orders.id')
+            ->leftJoin('lab_tests', 'lab_tests.id', '=', 'lab_order_tests.lab_test')
+            ->leftJoin('maritial_status', 'maritial_status.id', '=', 'patients.marital_status')
+            ->where('lab_orders.status', 1)
+            ->groupby('lab_orders.id')
+            ->get();
+
+
+        foreach ($orders as $lab_orders) {
+
+
+            $lab_orders->ordered_by = 'Dr Smith';
+            $lab_orders->handled_by = 'James';
+            $lab_orders->total_cost = 0;
+            //$lab_orders->test_name = 'Blood Test';
+
+
+            if ($lab_orders->sex == 1) {
+
+                $lab_orders->gender = 'male';
+            } else {
+
+                $lab_orders->gender = 'female';
+            }
+
+        }
+        foreach ($orders as $key => $order_tests) {
+            $tests = DB::table('lab_tests')
+                ->select(DB::raw('lab_tests.name as test_name,lab_tests.cost,priority'))
+                ->leftJoin('lab_order_tests', 'lab_order_tests.lab_test', '=', 'lab_tests.id')
+                ->where('lab_order_tests.lab_order_id', $order_tests->id)
+                ->get();
+
+            $test_cost = DB::table('lab_tests')
+                ->select(DB::raw('IFNULL(SUM(lab_tests.cost),0) as cost,count(lab_order_tests.lab_test) as totaltests'))
+                ->leftJoin('lab_order_tests', 'lab_order_tests.lab_test', '=', 'lab_tests.id')
+                ->where('lab_order_tests.lab_order_id', $order_tests->id)
+                ->first();
+
+            $orders[$key]->total_cost = $test_cost->cost;
+            $orders[$key]->total_test = $test_cost->totaltests;
+
+
+            $orders[$key]->lab_tests = $tests;
+
             //$apetizer_product_items[]=$product_item;
         }
 
@@ -133,22 +192,53 @@ class OrderController extends Controller
         $order_id = $request->input('order_id');
 
         $orders = DB::table('lab_orders')
-            ->select(DB::raw('lab_orders.id,lab_orders.patient_id,patients.first_name as patient_name,lab_orders.priority,lab_orders.order_status,labs.name as lab_name,patients.age,patients.marital_status,patients.sex,maritial_status.name as marital_status'))
+            ->select(DB::raw('lab_orders.id,lab_orders.patient_id,patients.first_name as patient_name,lab_orders.order_status,labs.name as lab_name,patients.age,patients.marital_status,patients.sex,maritial_status.name as marital_status'))
             ->leftJoin('patients', 'lab_orders.patient_id', '=', 'patients.id')
             ->leftJoin('labs', 'labs.id', '=', 'lab_orders.lab')
-            ->leftJoin('lab_tests', 'lab_tests.id', '=', 'lab_orders.lab_test')
+            ->leftJoin('lab_order_tests', 'lab_order_tests.lab_order_id', '=', 'lab_orders.id')
+            ->leftJoin('lab_tests', 'lab_tests.id', '=', 'lab_order_tests.lab_test')
             ->leftJoin('maritial_status', 'maritial_status.id', '=', 'patients.marital_status')
             ->where('lab_orders.status', 1)
+            ->groupby('lab_orders.id')
             ->where('lab_orders.id', $order_id)
             ->first();
+        if ($orders->sex == 1) {
 
-            if ($orders->sex == 1) {
+            $orders->gender = 'male';
+        } else {
 
-                $orders->gender = 'male';
-            } else {
+            $orders->gender = 'female';
+        }
 
-                $orders->gender = 'female';
-            }
+
+        $tests = DB::table('lab_tests')
+            ->select(DB::raw('lab_tests.name as test_name,lab_tests.cost,priority'))
+            ->leftJoin('lab_order_tests', 'lab_order_tests.lab_test', '=', 'lab_tests.id')
+            ->where('lab_order_tests.lab_order_id', $orders->id)
+            ->get();
+
+
+        $test_cost = DB::table('lab_tests')
+                 ->select(DB::raw('IFNULL(SUM(lab_tests.cost),0) as cost,count(lab_order_tests.lab_test) as totaltests'))
+                 ->leftJoin('lab_order_tests', 'lab_order_tests.lab_test', '=', 'lab_tests.id')
+                 ->where('lab_order_tests.lab_order_id', $orders->id)
+                 ->first();
+
+        if(!empty($test_cost)) {
+
+            $orders->total_cost = $test_cost->cost;
+            $orders->total_test = $test_cost->totaltests;
+
+        }
+
+        else{
+
+            $orders->total_cost = 0;
+            $orders->total_test = 0;
+
+        }
+        $orders->test = $tests;
+
 
 
 
@@ -249,9 +339,10 @@ class OrderController extends Controller
     }
 
 
-    public function get_lab_test_templates(Request $request){
+    public function get_lab_test_templates(Request $request)
+    {
 
-        $lab_templates= DB::table('lab_templates')
+        $lab_templates = DB::table('lab_templates')
             ->select(DB::raw('id,name'))
             ->where('status', '1')
             ->get();
