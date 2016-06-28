ef0cf5a40fba5b76c96c45a11ba3105594fdc67c
oneeb <oneebahmed@aplosinnovations.com>
2016-06-29 04:21:50 +0500

    Pharmacy View and controller

--- ng-app/controllers/pharmacy.js
+++ ng-app/controllers/pharmacy.js
@@ -1,6 +1,6 @@
 var AppEHR = angular.module('AppEHR');
 
-AppEHR.controller('pharmacy', ['$scope', '$rootScope', 'getPharmacy', '$window', '$routeParams','GetPatientInfo', function ($scope, $rootScope, getPharmacy, $window, $routeParams) {
+AppEHR.controller('pharmacy', ['$scope', '$rootScope', 'getPharmacy', '$window','Countries', 'States', '$routeParams','GetPatientInfo', function ($scope, $rootScope, getPharmacy, $window,Countries, States, $routeParams) {
 	$rootScope.pageTitle = "EHR - Pharmacy";
 	$scope.displayInfo = {};
 	getPharmacy.get({
@@ -9,13 +9,51 @@ AppEHR.controller('pharmacy', ['$scope', '$rootScope', 'getPharmacy', '$window',
 
 	function getPharmacySuccess(res) {
 		if (res.status == true) {
-			console.log(res.data);
 			$scope.pharmacyLists = res.data;
 		}
 	}
 
+
+
 	function getPharmacyFailure(error) {
 		console.log(error);
+	}
+	$scope.emailFormat = /^[a-z]+[a-z0-9._]+@[a-z]+\.[a-z.]{2,5}$/;
+
+
+	Countries.get({token: $window.sessionStorage.token}, countrySuccess, countryFailed);
+
+	function countrySuccess(res) {
+		if (res.status == true) {
+			$scope.countries = res.data;
+		} else {
+			console.log(res);
+		}
+	}
+
+	function countryFailed(error) {
+		console.log(error);
+	}
+
+	/*$scope.getStates=function(){
+		console.log($scope.countries)
+	}*/
+
+	$scope.addressStateByCountry = function (country) {
+		$scope.disabledDropdown = true;
+		States.get({token: $window.sessionStorage.token, country_id: country}, stateSuccess, stateFailed);
+
+		function stateSuccess(res) {
+			if (res.status == true && res.data.length > 0) {
+				angular.copy(res.data, $scope.state);
+				$scope.disabledDropdown = false;
+			}
+		}
+		function stateFailed(error) {
+			console.log(error);
+			console.log("here");
+		}
+
 	}
 
 }]);
