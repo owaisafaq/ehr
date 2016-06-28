42a5506c4f1173f4564740810145ea7060fbbbf7
Muzammil Ahmed <muzammilahmed@aplosinnovations.com>
2016-06-28 11:33:07 +0500

    pagination

--- ng-app/controllers/labOrderTests.js
+++ ng-app/controllers/labOrderTests.js
@@ -1,13 +1,11 @@
 var AppEHR = angular.module('AppEHR');
-// Lab Order Tests Listing
-AppEHR.controller('labOrderTests', ['$scope', '$rootScope','$window', '$routeParams','getLabOrderInfo','getLabTestInfo','$timeout', function ($scope, $rootScope, $window, $routeParams, getLabOrderInfo,getLabTestInfo, $timeout) {
+
+AppEHR.controller('labOrderTests', ['$scope', '$rootScope','$window', '$routeParams','getLabOrderInfo','$timeout', function ($scope, $rootScope, $window, $routeParams, getLabOrderInfo, $timeout) {
 	$rootScope.pageTitle = "EHR - Lab Order Test";
     $scope.action = "";
-    getLabOrderInfo.get({ // Getting all tests along with order info
-        token: $window.sessionStorage.token,
-        order_id: $routeParams.orderID},
-        getLabOrderInfoSuccess, getLabOrderInfoFailure);
+    getLabOrderInfo.get({token: $window.sessionStorage.token, order_id: $routeParams.orderID}, getLabOrderInfoSuccess, getLabOrderInfoFailure);
     function getLabOrderInfoSuccess(res) { // on success
+        console.log(res);
         if (res.status == true) {
             $rootScope.loader = "hide";
             $scope.orderSelected = true;
@@ -18,37 +16,5 @@ AppEHR.controller('labOrderTests', ['$scope', '$rootScope','$window', '$routePar
         $rootScope.loader = "show";
         console.log(error);
     }
-
-    $scope.testSelected = function (testID){ // For Selection of test
-        console.log(testID);
-        $scope.testID = testID;
-        $scope.selectedTest = {};
-        $rootScope.loader = "show";
-        getLabTestInfo.get({
-            token: $window.sessionStorage.token,
-            lab_test_id: testID
-        }, getLabTestInfoSuccess, getLabTestInfoFailure);
-        function getLabTestInfoSuccess(res) { // on success
-            if (res.status == true) {
-                $rootScope.loader = "hide";
-                $scope.testIsSelected = true;
-                $scope.selectedTest = res.data;
-            }
-        }
-        function getLabTestInfoFailure(error) { // on failure
-            $rootScope.loader = "show";
-            console.log(error);
-        }
-    }
-
-    $scope.search = function(item){ // search data by test name or test priority
-        if($scope.searchTest == undefined){
-            return true;
-        }else{
-            if(item.priority.toLowerCase().indexOf($scope.searchTest.toLowerCase()) != -1 || item.test_name.toLowerCase().indexOf($scope.searchTest.toLowerCase()) != -1){
-                return true;
-            }
-        }
-    };
 
 }]);
