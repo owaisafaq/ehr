5332b665d0d3cd42c7366b63c5e2c1e14296a41b
OwaisZaidi <owaiszaidi@aplosinnovations.com>
2016-07-28 14:18:14 +0500

    updated code ehr !

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -37,78 +37,51 @@ $app->group(['prefix' => 'api', 'middleware' => 'jwt.auth'], function () use ($a
 
 
     $app->post('register_patient', 'App\Http\Controllers\ApiController@register_patient');
-
-
     $app->post('add_patient', 'App\Http\Controllers\ApiController@add_patient');
-
     $app->post('delete_patient', 'App\Http\Controllers\ApiController@delete_patient');
-
     $app->post('add_patient_address', 'App\Http\Controllers\ApiController@add_patient_address');
-
     $app->post('add_patient_kin', 'App\Http\Controllers\ApiController@add_patient_kin');
-
     $app->post('add_patient_employees', 'App\Http\Controllers\ApiController@add_patient_employees');
 
     $app->post('add_visit' ,'App\Http\Controllers\ApiController@add_visit');
-
     $app->post('update_visit' ,'App\Http\Controllers\ApiController@update_visit');
 
     $app->get('get_countries' ,'App\Http\Controllers\ApiController@get_countries');
-
     $app->get('get_states' ,'App\Http\Controllers\ApiController@get_states');
-
     $app->get('get_cities' ,'App\Http\Controllers\ApiController@get_cities');
-
     $app->get('get_local_goverment_area' ,'App\Http\Controllers\ApiController@get_local_goverment_area');
-
     $app->get('get_dropdowndata' ,'App\Http\Controllers\ApiController@get_dropdowndata');
 
     $app->get('get_patient_vitals','App\Http\Controllers\ApiController@get_patient_vitals');
-
     $app->get('get_patient_plan','App\Http\Controllers\ApiController@get_patient_plan');
-
     $app->post('add_patient_plan','App\Http\Controllers\ApiController@add_patient_plan');
-
     $app->get('get_patient','App\Http\Controllers\ApiController@get_patient');
-
     $app->get('get_visits','App\Http\Controllers\ApiController@get_visits');
-
     $app->get('get_patient_all_data','App\Http\Controllers\ApiController@get_patient_all_data');
-
     $app->post('add_patient_vitals','App\Http\Controllers\ApiController@add_patient_vitals');
 
     $app->get('get_patient_visit_history','App\Http\Controllers\ApiController@get_patient_visit_history');
-
     $app->get('get_patient_vital_history','App\Http\Controllers\ApiController@get_patient_vital_history');
 
     $app->get('get_patient_vital_history','App\Http\Controllers\ApiController@get_patient_vital_history');
-
     $app->post('update_visit_status','App\Http\Controllers\ApiController@update_visit_status');
 
     $app->get('get_patient_demographics','App\Http\Controllers\ApiController@get_patient_demographics');
 
     $app->get('remove_visit','App\Http\Controllers\ApiController@remove_visit');
-
     $app->get('visit_details','App\Http\Controllers\ApiController@visit_details');
 
     $app->get('patient_archives','App\Http\Controllers\ApiController@patient_archives');
-
     $app->get('remove_patient_archive','App\Http\Controllers\ApiController@remove_patient_archive');
 
     $app->get('patient_medications','App\Http\Controllers\ApiController@patient_medications');
-
     $app->post('add_patient_medications','App\Http\Controllers\ApiController@add_patient_medications');
 
     $app->get('patient_supplements','App\Http\Controllers\ApiController@patient_supplements');
-
     $app->post('add_patient_supplements','App\Http\Controllers\ApiController@add_patient_supplements');
-
     $app->get('patient_allergies','App\Http\Controllers\ApiController@patient_allergies');
-
     $app->post('add_patient_allergies','App\Http\Controllers\ApiController@add_patient_allergies');
-    
     $app->post('update_patient_allergies','App\Http\Controllers\ApiController@update_patient_allergies');
-
     $app->post('delete_patient_allergies','App\Http\Controllers\ApiController@delete_patient_allergies');
 
     $app->get('patient_visit_list','App\Http\Controllers\ApiController@patient_visit_list');
@@ -116,31 +89,20 @@ $app->group(['prefix' => 'api', 'middleware' => 'jwt.auth'], function () use ($a
     $app->get('get_all_patients','App\Http\Controllers\ApiController@get_all_patients');
 
     $app->get('get_patient_appointments','App\Http\Controllers\ApiController@get_patient_appointments');
-
     $app->post('add_patient_appointments','App\Http\Controllers\ApiController@add_patient_appointments');
-
     $app->post('update_patient_appointments','App\Http\Controllers\ApiController@update_patient_appointments');
-
     $app->post('delete_patient_appointments','App\Http\Controllers\ApiController@delete_patient_appointments');
 
     $app->post('add_resources','App\Http\Controllers\ApiController@add_resources');
-
     $app->get('list_resources','App\Http\Controllers\ApiController@list_resources');
-
     $app->get('list_resources_back','App\Http\Controllers\ApiController@list_resources_back');
-
     $app->get('list_patient_resources','App\Http\Controllers\ApiController@list_patient_resources');
-
     $app->get('list_patient_resources_back','App\Http\Controllers\ApiController@list_patient_resources_back');
-
     $app->post('update_patient_resources','App\Http\Controllers\ApiController@update_patient_resources');
-
     $app->post('delete_patient_resources','App\Http\Controllers\ApiController@delete_patient_resources');
 
     $app->get('clinical_progress_note_templates','App\Http\Controllers\ApiController@clinical_progress_note_templates');
-
     $app->get('clinical_progress_note_fields','App\Http\Controllers\ApiController@clinical_progress_note_fields');
-
     $app->post('add_patient_clinical_notes','App\Http\Controllers\ApiController@add_patient_clinical_notes');
 
     $app->post('checkout_patient','App\Http\Controllers\ApiController@checkout_patient');
@@ -148,48 +110,29 @@ $app->group(['prefix' => 'api', 'middleware' => 'jwt.auth'], function () use ($a
     $app->post('add_patient_referel','App\Http\Controllers\ApiController@add_patient_referel');
 
     $app->get('get_lab_tests','App\Http\Controllers\OrderController@get_lab_tests');
-
     $app->get('get_lab_test_details','App\Http\Controllers\OrderController@get_lab_test_details');
-
     $app->get('get_all_lab_orders','App\Http\Controllers\OrderController@get_all_lab_orders');
-
     $app->get('get_patient_lab_orders','App\Http\Controllers\OrderController@get_patient_lab_orders');
-
     $app->get('get_lab_order','App\Http\Controllers\OrderController@get_lab_order');
-
     $app->post('add_lab_order','App\Http\Controllers\OrderController@add_lab_order');
-
     $app->post('cancel_lab_order','App\Http\Controllers\OrderController@cancel_lab_order');
-
     $app->post('update_lab_test','App\Http\Controllers\OrderController@update_lab_test');
-
     $app->get('get_lab_test_fields','App\Http\Controllers\OrderController@get_lab_test_fields');
-
     $app->post('add_lab_test_values','App\Http\Controllers\OrderController@add_lab_test_values');
-
     $app->get('get_lab_test_templates','App\Http\Controllers\OrderController@get_lab_test_templates');
-
     $app->post('add_lab_test_templates','App\Http\Controllers\OrderController@add_lab_test_templates');
-
     $app->post('update_lab_test_templates','App\Http\Controllers\OrderController@update_lab_test_templates');
-
     $app->post('delete_lab_test_templates','App\Http\Controllers\OrderController@delete_lab_test_templates');
 
 
     $app->get('get_pharmacies','App\Http\Controllers\OtherController@get_pharmacies');
-
     $app->get('get_single_pharmacy','App\Http\Controllers\OtherController@get_single_pharmacy');
-
     $app->post('create_pharmacy','App\Http\Controllers\OtherController@create_pharmacy');
-
     $app->post('update_pharmacy','App\Http\Controllers\OtherController@update_pharmacy');
-
     $app->post('delete_pharmacy','App\Http\Controllers\OtherController@delete_pharmacy');
 
     $app->post('add_manufacturer','App\Http\Controllers\ApiController@add_manufacturer');
-
     $app->get('get_frequency','App\Http\Controllers\ApiController@get_frequency');
-
     $app->get('get_intake','App\Http\Controllers\ApiController@get_intake');
 
     $app->get('get_lab_order_history','App\Http\Controllers\OrderController@get_lab_order_history');
