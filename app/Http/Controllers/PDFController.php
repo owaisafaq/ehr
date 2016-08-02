e235bba59990e22b7c200671498fa1367985a677
OwaisZaidi <owaiszaidi@aplosinnovations.com>
2016-08-02 17:08:18 +0500

    updated code ehr !

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -34,21 +34,35 @@ class PDFController extends Controller
         foreach ($template->fields as $temp){
             foreach ($template_values as $k => $v){
                 if($temp->name == $k){
-                    array_push($arr, ['value'=> $v,'field' =>$temp]);
+                    array_push($arr, ['value'=> $v,'field' =>$temp,]);
                 }
             }
         }
-//    print_r($arr);
-//    print_r($template_values);
-//    print_r($template);
-//    print_r($template);
-//    echo $template->fields[0]->displayName;
-//
+        $logo_image = url('/') . '/uploaded_images/';
+
+
+        //    print_r($arr);
+        //    print_r($template_values);
+        //    print_r($template);
+        //    print_r($template);
+        //    echo $template->fields[0]->displayName;
+
+
+        // All The Patient info is here just uncomment it when required
+
+/*        $patient = DB::table('patient_lab_test_values')
+             ->select(DB::raw('patients.id,CONCAT(patients.first_name," ",patients.last_name) AS patient_name,CONCAT("' . $logo_image . '",patients.patient_image) as patient_image,patients.age,patients.date_of_birth,maritial_status.name as marital_status,(CASE WHEN (sex = 1) THEN "Male" ELSE "Female" END) as gender'))
+             ->leftJoin('lab_orders', 'lab_orders.id', '=', 'patient_lab_test_values.lab_order_id')
+             ->leftJoin('patients', 'patients.id', '=', 'lab_orders.patient_id')
+             ->leftJoin('maritial_status', 'patients.marital_status', '=', 'maritial_status.id')
+             ->where('lab_test', $id)->first();
+
+       array_push($arr,['patient'=>$patient]);*/
+
         $data = ['data'=>$arr];
 
-
         $view =  app()->make('view')->make('report_pdf', $data)->render();
-//        return $view;
+//    return $view;
 //    return $view;
 
         $pdf = PDF::loadHTML($view);
@@ -61,10 +75,10 @@ class PDFController extends Controller
         $file_archive = url('/').'/patient_archive/report.pdf';
 
         echo json_encode(array(
-                       'status' => true,
-                       'data' => $file_archive,
+            'status' => true,
+            'data' => $file_archive,
 
-                   ),JSON_UNESCAPED_SLASHES);
+        ), JSON_UNESCAPED_SLASHES);
 
     }
 
