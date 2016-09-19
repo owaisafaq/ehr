98b78e851e9b422dd84c68874f86c4dee9a01368
OwaisZaidi <owaiszaidi@aplosinnovations.com>
2016-09-19 19:37:35 +0500

    updated code ehr !

--- app/Http/Controllers/OtherController.php
+++ app/Http/Controllers/OtherController.php
@@ -489,7 +489,7 @@ class OtherController extends Controller
             $beds = DB::table('beds')
                 ->leftJoin('patients_admitted', 'beds.patient_id', '=', 'patients_admitted.patient_id')
                 ->leftJoin('patients', 'patients.id', '=', 'patients_admitted.patient_id')
-                ->select(DB::raw('beds.id,beds.bed_status,patients.first_name,patients.middle_name,patients.last_name,patients.date_of_birth,patients.sex,patients_admitted.expected_discharge_date'))
+                ->select(DB::raw('beds.id as patient_bed_id,beds.bed_status,patients.first_name,patients.middle_name,patients.last_name,patients.date_of_birth,patients.sex,patients_admitted.expected_discharge_date'))
                 ->where('beds.status', 1)
                 ->where('beds.ward_id', $ward_id)
                 ->get();
