85f97f3451c18d71659a20110bcab8169028e445
Muzammil Ahmed <muzammilahmed@aplosinnovations.com>
2016-10-19 15:01:11 +0500

    Team work tasks

--- ng-app/controllers/billing.js
+++ ng-app/controllers/billing.js
@@ -442,7 +442,11 @@ AppEHR.controller('billing', ['$scope', '$rootScope','$window','$routeParams','$
         function addToBillSuccess(res){
         	$rootScope.loader = "hide";
         	if(res.status == true){
-        		$('#addToBill').modal('show');
+        		$('#addToBill').modal('hide');
+        		GetAllInvoices.get({
+					token: $window.sessionStorage.token,
+			        bill_id : $scope.bill_id
+				}, GetAllInvoicesSuccess, GetAllInvoicesFailure);
         	}
         }
         GetAllproducts.save({token: $window.sessionStorage.token}, getAllPbillSuccess, checkoutSuccessFailure);
