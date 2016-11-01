e3573a1cf7cd29111db4735bb3b5f7a374907af0
OwaisZaidi <owaiszaidi@aplosinnovations.com>
2016-11-01 15:22:41 +0500

    updated code ehr

--- test_api/api.array.php
+++ test_api/api.array.php
@@ -1,9 +1,9 @@
 <?php
 
 require 'api.class.php';
-define('HOST', 'http://131.107.100.10/ehr/public/api/');
+//define('HOST', 'http://131.107.100.10/ehr/public/api/');
 //define('HOST', 'http://localhost/ehr/public/api/');
-//define('HOST', 'http://demoz.online/ehr/public/api/');
+define('HOST', 'http://demoz.online/ehr/public/api/');
 //define('HOST', 'http://demoz.online/dev/ehr/public/api/');
 define('APP', '');
 define('ROUTE', '');
