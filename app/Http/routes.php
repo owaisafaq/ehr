9fec08ced51411b6d03cff009ad22474caed25e3
OwaisZaidi <owaiszaidi@aplosinnovations.com>
2016-11-10 20:26:36 +0500

    updated code ehr

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -500,5 +500,6 @@ $app->group(['prefix'=>'api','middleware' => 'jwt.auth'], function () use ($app)
     $app->post('add_patient_bill','App\Http\Controllers\BillingController@add_patient_bill');
     $app->post('search_patient_bill','App\Http\Controllers\BillingController@search_patient_bill');
     $app->post('add_prescription_material','App\Http\Controllers\OtherController@add_prescription_material');
+    $app->get('download_patient_referal','App\Http\Controllers\PDFController@download_patient_referal');
 
 });
